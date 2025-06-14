426e765e6be622adfe57f84ee77166a0c10b515d
saurabhsingh <saurabhsingh6844@gmail.com>
2025-06-14 22:37:54 +0530

    How to use packages

---
+++ src/in/kgcoding/geometry/Rectangle.java
@@ -0,0 +1,12 @@
+package in.kgcoding.geometry;
+
+public class Rectangle {
+
+    public double length;
+    public double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+}
---
+++ src/in/kgcoding/utils/Calculator.java
@@ -0,0 +1,17 @@
+package in.kgcoding.utils;
+
+import in.kgcoding.geometry.Circle;
+import in.kgcoding.geometry.Rectangle;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Circle cir = new Circle(5.5);
+        Rectangle rect = new Rectangle(5,10);
+
+
+        double cirArea = Math.PI * Math.pow(cir.radius,2);
+        double rectArea = rect.length*rect.width;
+
+        System.out.printf("Area of circle is : %f , and Area of Rectangle is : %f",cirArea, rectArea);
+    }
+}
